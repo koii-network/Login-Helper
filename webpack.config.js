d3f1edb98a93abfa60f37c4fa563a0fb021b70d8
Herman Liang <herman@koii.network>
2024-08-20 16:54:48 -0300

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+const nodeExternals = require("webpack-node-externals");
 module.exports={
     entry:"./index.js",
     target: 'node',
     // When uploading to arweave use the production mode
     // mode:"production",
     mode: "development",
+    externalsPresets: { node: true },   // <-- here
+    externals: [nodeExternals()],       // <-- and here
     devtool: 'source-map',
     optimization: {
         usedExports: false, // <- no remove unused function
