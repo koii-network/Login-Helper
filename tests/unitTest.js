e77ef907b6383b98b89aaeea4ce82f61f259b049
Soma Li <soma@koii.network>
2024-08-28 13:42:35 -0300

    update common login task

--- tests/unitTest.js
+++ tests/unitTest.js
@@ -6,12 +6,12 @@ function sleep(ms) {
 
 async function executeTasks() {
     // i is the round number
-
-        let delay = 6000;
+    for (let i = 0; i < 5; i++) {
+        let delay = 900000;
         let round = 0;
         await coreLogic.task(round);
         await sleep(delay);
-
+    }
     console.log('All tasks executed. Test completed.')
     process.exit(0);
 }
